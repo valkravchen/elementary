95e0784f01a265117ddf51b8982198c095e4ed31
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-09-20 21:26:29 +0300

    1.6. Аргументы

---
+++ src/main/java/ru/calculator/ArgMethod.java
@@ -0,0 +1,16 @@
+package ru.calculator;
+
+public class ArgMethod {
+    public static void hello(String name, int age) {
+        System.out.println("Hello, " + name + ", age = " + age);
+    }
+
+    public static void main(String[] args) {
+        String name = "Java";
+        int age = 6;
+        ArgMethod.hello(name, age);
+        ArgMethod.hello(name, age);
+        ArgMethod.hello(name, age);
+        ArgMethod.hello(name, age);
+    }
+}
