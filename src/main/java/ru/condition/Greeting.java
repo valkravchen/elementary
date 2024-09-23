e230130b0201797cd7de0c8a585b77af82ea8c46
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-09-23 11:13:41 +0300

    1.14. String

---
+++ src/main/java/ru/condition/Greeting.java
@@ -0,0 +1,13 @@
+package ru.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        System.out.println(idea);
+        idea += " But I am a newbie. ";
+        System.out.println(idea);
+        int year = 2024;
+        idea += year;
+        System.out.println(idea);
+    }
+}
