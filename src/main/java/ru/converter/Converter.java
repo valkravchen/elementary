c174ffc9d573f740ccb73c82ba5f918428212685
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-09-24 22:54:09 +0300

    2. Что такое тестирование

--- src/main/java/ru/converter/Converter.java
+++ src/main/java/ru/converter/Converter.java
@@ -2,19 +2,23 @@ package ru.converter;
 
 public class Converter {
     public static float rubleToEuro(float value) {
-        float rsl = value / 100;
-        return rsl;
+        return value / 100;
     }
 
     public static float rubleToDollar(float value) {
-        float rsl = value / 90;
-        return rsl;
+        return value / 90;
     }
 
     public static void main(String[] args) {
-        float euro = Converter.rubleToEuro(200);
-        float dollar = Converter.rubleToDollar(270);
-        System.out.println("200 rubles are " + euro + " euro.");
-        System.out.println("270 rubles are " + dollar + " dollars.");
+        float inRubleToEuro = 140;
+        float expectedRubleToEuro  = 1.4f;
+        float outRubleToEuro  = Converter.rubleToEuro(inRubleToEuro);
+        boolean passedRubleToEuro  = expectedRubleToEuro == outRubleToEuro;
+        System.out.println("140 rubles are 1.4. Test result : " + passedRubleToEuro);
+        float inRubleToDollar = 180;
+        float expectedRubleToDollar = 2.0f;
+        float outRubleToDollar = Converter.rubleToDollar(inRubleToDollar);
+        boolean passedRubleToDollar   = expectedRubleToDollar == outRubleToDollar;
+        System.out.println("180 rubles are 2.0. Test result : " + passedRubleToDollar);
     }
 }
