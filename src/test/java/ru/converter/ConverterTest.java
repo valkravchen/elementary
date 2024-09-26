91de8519100041750dc8b11755613992ab4b8520
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-09-26 21:35:04 +0300

    2.1. Модульные тесты

--- src/test/java/ru/converter/ConverterTest.java
+++ src/test/java/ru/converter/ConverterTest.java
@@ -23,4 +23,38 @@ class ConverterTest {
         float out = Converter.rubleToDollar(in);
         assertThat(out).isEqualTo(expected);
     }
+
+    @Test
+    void whenConvert0RblThen0Euro() {
+        float in = 0;
+        float expected = 0;
+        float out = Converter.rubleToEuro(in);
+        assertThat(out).isEqualTo(expected);
+    }
+
+    @Test
+    void whenConvert0RblThen0Dlr() {
+        float in = 0;
+        float expected = 0;
+        float out = Converter.rubleToDollar(in);
+        assertThat(out).isEqualTo(expected);
+    }
+
+    @Test
+    void whenConvert333RblThen3Dot33Euro() {
+        float in = 333;
+        float expected = 3.33f;
+        float out = Converter.rubleToEuro(in);
+        float eps = 0.0001f;
+        assertThat(out).isEqualTo(expected, withPrecision(eps));
+    }
+
+    @Test
+    void whenConvert333RblThen3Dot7Dlr() {
+        float in = 333;
+        float expected = 3.7f;
+        float out = Converter.rubleToDollar(in);
+        float eps = 0.0001f;
+        assertThat(out).isEqualTo(expected, withPrecision(eps));
+    }
 }
