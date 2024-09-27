9e03fe95529194555f01eac9588e4cddf07c37cb
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-09-27 21:40:59 +0300

    2.5. Тесты для задания "Расстояние между точками в системе координат"

---
+++ src/test/java/ru/condition/PointTest.java
@@ -0,0 +1,41 @@
+package ru.condition;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class PointTest {
+
+    @Test
+    void when00to20then2() {
+        double expected = 2;
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 2;
+        int y2 = 0;
+        double out = Point.distance(x1, y1, x2, y2);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void when11to11then2() {
+        double expected = 0;
+        int x1 = 1;
+        int y1 = 1;
+        int x2 = 1;
+        int y2 = 1;
+        double out = Point.distance(x1, y1, x2, y2);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void when23to73then5() {
+        double expected = 5;
+        int x1 = 2;
+        int y1 = 3;
+        int x2 = 7;
+        int y2 = 3;
+        double out = Point.distance(x1, y1, x2, y2);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+}
