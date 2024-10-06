d7e8dcf8dee22c42b9059610def4b0ee93a9f24a
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-10-06 17:40:40 +0300

    3.13. Множественное логическое выражение с оператором "И"

--- src/main/java/ru/condition/Triangle.java
+++ src/main/java/ru/condition/Triangle.java
@@ -2,7 +2,7 @@ package ru.condition;
 
 public class Triangle {
     public static boolean exist(double ab, double ac, double bc) {
-        return (ab + ac) > bc && (ab + bc) > ac && (ac + bc) > ab;
+        return ab + ac > bc && ab + bc > ac && ac + bc > ab;
     }
 
     public static void main(String[] args) {
