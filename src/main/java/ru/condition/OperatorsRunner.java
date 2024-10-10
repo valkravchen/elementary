b1f956f13c50b5f1f53c8f369591048ab0891f21
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-10-10 19:24:08 +0300

    OperatorsRunner

---
+++ src/main/java/ru/condition/OperatorsRunner.java
@@ -0,0 +1,11 @@
+package ru.condition;
+
+public class OperatorsRunner {
+    public static void main(String[] args) {
+        int i = 10;
+        i = i + 1;
+        i += 1;
+        i++;
+        System.out.println(i);
+    }
+}
