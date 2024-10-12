91d6384c19cef48322670a7b54227659d69711f7
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-10-12 09:25:25 +0300

    3.18. Switch - matcher

--- src/main/java/ru/condition/MultipleSwitchWeek.java
+++ src/main/java/ru/condition/MultipleSwitchWeek.java
@@ -2,19 +2,22 @@ package ru.condition;
 
 public class MultipleSwitchWeek {
     public static int numberOfDay(String name) {
-        return switch (name) {
-            case "Понедельник", "Monday" -> 1;
-            case "Вторник", "Tuesday" -> 2;
-            case "Среда", "Wednesday" -> 3;
-            case "Четверг", "Thursday" -> 4;
-            case "Пятница", "Friday" -> 5;
-            case "Суббота", "Saturday" -> 6;
-            case "Воскресенье", "Sunday" -> 7;
+        String lowerCaseName = name.toLowerCase();
+        return switch (lowerCaseName) {
+            case "понедельник", "monday" -> 1;
+            case "вторник", "tuesday" -> 2;
+            case "среда", "wednesday" -> 3;
+            case "четверг", "thursday" -> 4;
+            case "пятница", "friday" -> 5;
+            case "суббота", "saturday" -> 6;
+            case "воскресенье", "sunday" -> 7;
             default -> -1;
         };
     }
 
     public static void main(String[] args) {
         System.out.println(numberOfDay("понедельник"));
+        System.out.println(numberOfDay("вТорник"));
+        System.out.println(numberOfDay("WEDNESDAY"));
     }
 }
