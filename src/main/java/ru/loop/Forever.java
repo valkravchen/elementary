b126937c87de4c22d88b6b32395db208389eecb1
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-10-14 17:11:41 +0300

    Forever

---
+++ src/main/java/ru/loop/Forever.java
@@ -0,0 +1,9 @@
+package ru.loop;
+
+public class Forever {
+    public static void main(String[] args) {
+        for (int index = 0; index < 10; index--) {
+            System.out.println("Forever");
+        }
+    }
+}
