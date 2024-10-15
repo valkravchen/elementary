e846870dc110e1195309381c8aa757f874796616
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-10-15 19:07:13 +0300

    4.4. Подсчёт суммы

--- src/main/java/ru/loop/Counter.java
+++ src/main/java/ru/loop/Counter.java
@@ -1,6 +1,14 @@
 package ru.loop;
 
 public class Counter {
+    public static int sum(int start, int finish) {
+        int sum = 0;
+        for (int index = start; index <= finish; index++) {
+            sum += index;
+        }
+        return sum;
+    }
+
     public static int sumByEven(int start, int finish) {
         int sum = 0;
         for (int index = start; index <= finish; index++) {
