138fdbde43c8bb11ab5011fa1e32cf8b3a3ce3aa
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-10-15 19:18:16 +0300

    4.5. Факториал

--- src/main/java/ru/loop/Factorial.java
+++ src/main/java/ru/loop/Factorial.java
@@ -3,9 +3,6 @@ package ru.loop;
 public class Factorial {
     public static int calc(int n) {
         int result = 1;
-        if (n == 0) {
-            return result;
-        }
         for (int index = 1; index <= n; index++) {
             result *= index;
         }
