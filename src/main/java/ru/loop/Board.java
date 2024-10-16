72dd1ce40db9898e0e2c019f3c419ea878370359
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-10-16 17:57:39 +0300

    Board

---
+++ src/main/java/ru/loop/Board.java
@@ -0,0 +1,20 @@
+package ru.loop;
+
+public class Board {
+    public static void paint(int width, int height) {
+        for (int row = 0; row < height; row++) {
+            for (int cell = 0; cell < width; cell++) {
+                /* печатаем X - width раз. */
+                System.out.print("X");
+            }
+            /* добавляем перевод на новую строку. */
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        paint(3, 3);
+        System.out.println();
+        paint(4, 4);
+    }
+}
