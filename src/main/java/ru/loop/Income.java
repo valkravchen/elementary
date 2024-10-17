b62f053b7cebf110bfbbbf00f98841cf736fe29a
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-10-17 18:48:28 +0300

    Цикл while

---
+++ src/main/java/ru/loop/Income.java
@@ -0,0 +1,11 @@
+package ru.loop;
+
+public class Income {
+    public static void main(String[] args) {
+        int income = 0;
+        while (income < 100) {
+            System.out.println(income);
+            income += 10;
+        }
+    }
+}
