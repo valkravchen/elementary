aede139e5f3de60d963edaa78e7f54c2c3489931
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-10-19 15:22:08 +0300

    4.8. Простое число и оператор прерывания break

---
+++ src/main/java/ru/loop/CheckPrimeNumber.java
@@ -0,0 +1,14 @@
+package ru.loop;
+
+public class CheckPrimeNumber {
+    public static boolean check(int number) {
+        int result = 0;
+        for (int index = 1; index <= number; index++) {
+            if (number % index == 0) {
+                result++;
+            }
+        }
+        return result == 2;
+    }
+}
+
