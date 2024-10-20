e39b796741cc258f88e9165b6b69768bfb158da5
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-10-20 09:17:57 +0300

    4.10. Ипотека

--- src/main/java/ru/loop/Mortgage.java
+++ src/main/java/ru/loop/Mortgage.java
@@ -4,17 +4,9 @@ public class Mortgage {
     public static int year(double amount, double salary, double percent) {
         int year = 0;
         while (amount > 0) {
-            amount += amount * percent;
-            amount -= salary;
+            amount += amount * (percent / 100) - salary;
             year++;
         }
         return year;
-    }
-
-    public static void main(String[] args) {
-        double amount = 100;
-        double percent = 0.5;
-        double salary = 70;
-        System.out.println(year(amount, salary, percent));
     }
 }
