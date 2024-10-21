534e3af4c471e7253ad30c60a2aee9b95362291a
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-10-21 21:09:07 +0300

    sumUpTo

---
+++ src/main/java/vaok/tasks/loops/SumNumbers.java
@@ -0,0 +1,20 @@
+package vaok.tasks.loops;
+
+public class SumNumbers {
+    public static void sumUpTo(int number) {
+        int sum = 0;
+        int counter = 1;
+
+        // Цикл while для суммирования чисел
+        while (counter <= number) {
+            sum += counter;
+            counter++;
+        }
+
+        System.out.println("Сумма чисел от 1 до " + number + " равна " + sum);
+    }
+
+    public static void main(String[] args) {
+        sumUpTo(100); // Выводим сумму чисел от 1 до 100
+    }
+}
