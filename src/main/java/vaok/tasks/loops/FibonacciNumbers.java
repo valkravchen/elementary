311855ca71280f4dc641093cd52bdbad2000570a
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-10-24 19:57:48 +0300

    FibonacciNumbers

---
+++ src/main/java/vaok/tasks/loops/FibonacciNumbers.java
@@ -0,0 +1,23 @@
+package vaok.tasks.loops;
+
+public class FibonacciNumbers {
+    public static void printFibonacciUpTo(int number) {
+        int previous = 0;
+        int current = 1;
+
+        System.out.println(previous);
+        while (current <= number) {
+            System.out.println(current);
+            int next = previous + current;
+            previous = current;
+            current = next;
+        }
+    }
+
+    public static void main(String[] args) {
+        printFibonacciUpTo(20);
+    }
+}
+
+
+
