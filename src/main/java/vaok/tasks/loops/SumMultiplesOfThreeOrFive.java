01922a141848c22bc2cbf53da0bc252046153607
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-10-25 21:50:06 +0300

    calculateSum

---
+++ src/main/java/vaok/tasks/loops/SumMultiplesOfThreeOrFive.java
@@ -0,0 +1,18 @@
+package vaok.tasks.loops;
+
+public class SumMultiplesOfThreeOrFive {
+    public static void calculateSum(int number) {
+        int sum = 0;
+        for (int index = 1; index <= number; index++) {
+            if (index % 3 == 0 || index % 5 == 0) {
+                sum += index;
+            }
+        }
+        System.out.println(sum);
+    }
+
+    public static void main(String[] args) {
+        calculateSum(45);
+    }
+}
+
