107f3222ade3e071cbcd1f75e00b737b23fc1fff
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-10-26 20:09:52 +0300

    Шаблоны в IntelliJ IDEA

--- src/main/java/ru/live/Main.java
+++ src/main/java/ru/live/Main.java
@@ -2,8 +2,12 @@ package ru.live;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
-        for (int i = 0; i < ; i++) {
+        for (int i = 1; i < 4; i++) {
+            System.out.println(i);
+            for (int j = 1; j < 4; j++) {
+                String s = " ";
+                System.out.println(s + j);
+            }
         }
     }
 }
