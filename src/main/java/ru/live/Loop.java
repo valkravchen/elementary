50e5705f076f4ea4459cc27e6b1f0e87faebc060
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-10-26 20:20:14 +0300

    4.14. Генерация конструкций в IntelliJ IDEA

---
+++ src/main/java/ru/live/Loop.java
@@ -0,0 +1,9 @@
+package ru.live;
+
+public class Loop {
+    public static void main(String[] args) {
+        for (int index = 0; index < 11; index++) {
+            System.out.println(index);
+        }
+    }
+}
