549beb98fa732cf3f58184546b805515c8b6c080
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-10-27 15:58:40 +0300

    5.13. Заполнение массива (теория)

---
+++ src/main/java/ru/array/Nums.java
@@ -0,0 +1,13 @@
+package ru.array;
+
+public class Nums {
+    public static void main(String[] args) {
+        int[] nums = new int[5];
+        nums[0] = 2006;
+        nums[1] = 2010;
+        nums[2] = 2014;
+        nums[3] = 2018;
+        nums[4] = 2022;
+        System.out.println(nums[3]);
+    }
+}
