0a8a3b3dd48871ad3bde8aa95c8987feae85c8e1
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-10-27 20:51:21 +0300

    5.4. Массивы и цикл for

--- src/main/java/ru/array/ArrayLoop.java
+++ src/main/java/ru/array/ArrayLoop.java
@@ -5,7 +5,7 @@ public class ArrayLoop {
         int[] numbers = new int[5];
         for (int index = 0; index < numbers.length; index++) {
             numbers[index] = index * 2 + 3;
-            System.out.println(numbers[index]);
+            System.out.println("Индекс " + index + ": " + numbers[index]);
         }
     }
 }
