9a5294316da2d543e1f7ef47797761b20e0f41b0
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-10-27 09:55:08 +0300

    5.2. Размер массива (теория)

---
+++ src/main/java/ru/array/ArraySizeDemo.java
@@ -0,0 +1,14 @@
+package ru.array;
+
+public class ArraySizeDemo {
+    public static void main(String[] args) {
+        int[] ints = new int[10];
+        System.out.println("Размер массива равен: " + ints.length);
+
+        // Вывод начальных значений массива
+        System.out.println("Начальные значения массива:");
+        for (int i = 0; i < ints.length; i++) {
+            System.out.println("ints[" + i + "] = " + ints[i]);
+        }
+    }
+}
