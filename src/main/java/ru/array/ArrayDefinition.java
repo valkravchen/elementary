d797b4c138732e00d26c13b4f1bda8c6b0bd121e
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-10-27 06:41:23 +0300

    5.1. Объявление массива

---
+++ src/main/java/ru/array/ArrayDefinition.java
@@ -0,0 +1,9 @@
+package ru.array;
+
+public class ArrayDefinition {
+    public static void main(String[] args) {
+        short[] ages = new short[10];
+        String[] surnames = new String[10050];
+        float[] prices = new float[40];
+    }
+}
