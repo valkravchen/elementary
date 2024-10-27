c9ea60f36c81e54eca20b358268cdca023d3b998
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-10-27 19:56:33 +0300

    Заполнение массива через цикл

--- src/main/java/ru/array/ArrayDefinition.java
+++ src/main/java/ru/array/ArrayDefinition.java
@@ -2,14 +2,13 @@ package ru.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        String[] names = new String[4];
-        names[0] = "Д’Артаньян";
-        names[1] = "Атос";
-        names[2] = "Портос";
-        names[3] = "Арамис";
-
-        for (int i = 0; i < names.length; i++) {
-            System.out.println("Индекс " + i + ": " + names[i]);
+        int[] numbers = new int[10];
+        for (int index = 0; index < numbers.length; index++) {
+            numbers[index] = index;
+        }
+        for (int index = 0; index < numbers.length; index++) {
+            System.out.println(numbers[index]);
         }
     }
 }
+
