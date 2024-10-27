f3fc0bcb911bc6b1259de800ee96d29d451d8383
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-10-27 19:12:11 +0300

    5.4. Массивы и цикл for (теория)

--- src/main/java/ru/array/ArrayDefinition.java
+++ src/main/java/ru/array/ArrayDefinition.java
@@ -2,20 +2,14 @@ package ru.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        short[] ages = new short[10];
-        String[] surnames = new String[10050];
-        float[] prices = new float[40];
         String[] names = new String[4];
         names[0] = "Д’Артаньян";
         names[1] = "Атос";
         names[2] = "Портос";
         names[3] = "Арамис";
-        System.out.println("Размер массива ages равен: " + ages.length);
-        System.out.println("Размер массива surnames равен: " + surnames.length);
-        System.out.println("Размер массива prices равен: " + prices.length);
-        System.out.println(names[0]);
-        System.out.println(names[1]);
-        System.out.println(names[2]);
-        System.out.println(names[3]);
+
+        for (int index = 0; index < names.length; index++) {
+            System.out.println(names[index]);
+        }
     }
 }
