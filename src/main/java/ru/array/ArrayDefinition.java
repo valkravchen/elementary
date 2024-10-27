3628c02205ed0afaf8636fea574486f3781e4898
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-10-27 13:03:39 +0300

    5.2. Размер массива

--- src/main/java/ru/array/ArrayDefinition.java
+++ src/main/java/ru/array/ArrayDefinition.java
@@ -5,8 +5,9 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[10050];
         float[] prices = new float[40];
-        System.out.println("Размер массива равен: " + ages.length);
-        System.out.println("Размер массива равен: " + surnames.length);
-        System.out.println("Размер массива равен: " + prices.length);
+
+        System.out.println("Размер массива ages равен: " + ages.length);
+        System.out.println("Размер массива surnames равен: " + surnames.length);
+        System.out.println("Размер массива prices равен: " + prices.length);
     }
 }
