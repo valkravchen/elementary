0cfd5da45c389480b39d7279faade55c6468ed20
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-10-28 20:28:27 +0300

    5.5. Массивы и цикл for-each (теория)

---
+++ src/main/java/ru/array/ForEachUsage.java
@@ -0,0 +1,10 @@
+package ru.array;
+
+public class ForEachUsage {
+    public static void main(String[] args) {
+        String[] numbers = {"one", "two", "three", "four", "five"};
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
