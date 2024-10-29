77c687de930b8693ab2f1fba92431b8bcdfb9399
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-10-29 20:57:33 +0300

    Реализован метод calculateSumOfSquares для суммы квадратов

---
+++ src/main/java/vaok/tasks/loops/SumOfSquares.java
@@ -0,0 +1,15 @@
+package vaok.tasks.loops;
+
+public class SumOfSquares {
+    public static int calculateSumOfSquares(int number) {
+        int sum = 0;
+        for (int i = 1; i <= number; i++) {
+            sum += i * i; // добавляем квадрат текущего числа к сумме
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calculateSumOfSquares(3)); // Ожидаемый результат: 14
+    }
+}
