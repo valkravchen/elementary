f7789594fc7f03e6b561a41b83bbfe914586123d
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-11-01 21:19:48 +0300

    Реализован метод calculateOddSum для нахождения суммы нечётных чисел

---
+++ src/main/java/vaok/tasks/loops/OddNumberSumCalculator.java
@@ -0,0 +1,17 @@
+package vaok.tasks.loops;
+
+public class OddNumberSumCalculator {
+    public static int calculateOddSum(int number) {
+        int sum = 0;
+        for (int i = 1; i <= number; i += 2) { // проходим только по нечётным числам
+            sum += i;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calculateOddSum(10)); // Ожидаемый результат: 25
+        System.out.println(calculateOddSum(15)); // Ожидаемый результат: 64
+    }
+}
+
