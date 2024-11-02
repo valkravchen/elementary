103e9b3aabb5fcd86b1245f4988123c80159317d
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-11-02 20:26:47 +0300

    Реализован метод calculateFactorial для вычисления факториала числа

---
+++ src/main/java/vaok/tasks/loops/FactorialCalculator.java
@@ -0,0 +1,16 @@
+package vaok.tasks.loops;
+
+public class FactorialCalculator {
+    public static int calculateFactorial(int number) {
+        int factorial = 1;
+        for (int i = 1; i <= number; i++) {
+            factorial *= i;
+        }
+        return factorial;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calculateFactorial(5)); // Ожидаемый результат: 120
+        System.out.println(calculateFactorial(3)); // Ожидаемый результат: 6
+    }
+}
