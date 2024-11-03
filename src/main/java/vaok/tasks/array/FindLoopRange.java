48353585c423e451eeff392f1208a2fc3c7399fe
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-11-03 11:11:35 +0300

    Реализован метод indexOfInRange для поиска элемента в заданном диапазоне массива

---
+++ src/main/java/vaok/tasks/array/FindLoopRange.java
@@ -0,0 +1,21 @@
+package vaok.tasks.array;
+
+public class FindLoopRange {
+    public static int indexOfInRange(int[] data, int element, int start, int end) {
+        for (int index = start; index <= end; index++) {
+            if (data[index] == element) {
+                return index;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] data = {1, 3, 6, 8, 10};
+        int element = 3;
+        int start = 0;
+        int end = 3;
+        System.out.println(indexOfInRange(data, element, start, end));
+    }
+}
+
