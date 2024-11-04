df03a7a4bf592f03ddc5d37677f2110ec0980d3f
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-11-04 19:18:49 +0300

    5.9. Упорядочить массив

---
+++ src/main/java/ru/array/AlgoArray.java
@@ -0,0 +1,19 @@
+package ru.array;
+
+public class AlgoArray {
+    public static void main(String[] args) {
+        int[] array = new int[] {5, 3, 2, 1, 4};
+        int tempIndex0 = array[0];
+        array[0] = array[3];
+        int tempIndex1 = array[1];
+        array[1] = array[2];
+        array[2] = tempIndex1;
+        array[3] = array[4];
+        array[4] = tempIndex0;
+
+        for (int number : array) {
+            System.out.println(number);
+        }
+    }
+}
+
