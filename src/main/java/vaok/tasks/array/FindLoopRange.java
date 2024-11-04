4f3a9fe889083712f1a3314e2efbf068485192e5
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-11-04 05:22:59 +0300

    Реализован метод indicesOfGreaterThan для поиска и возврата индексов элементов, превышающих заданное значение

--- src/main/java/vaok/tasks/array/FindLoopRange.java
+++ src/main/java/vaok/tasks/array/FindLoopRange.java
@@ -1,5 +1,7 @@
 package vaok.tasks.array;
 
+import java.util.Arrays;
+
 public class FindLoopRange {
     public static int indexOfInRange(int[] data, int element, int start, int end) {
         if (start < 0 || end >= data.length || start > end) {
@@ -13,12 +15,27 @@ public class FindLoopRange {
         return -1;
     }
 
+    public static int[] indicesOfGreaterThan(int[] data, int element, int start, int end) {
+        int[] indices = new int[end - start + 1];
+        int count = 0;
+        if (start < 0 || end >= data.length || start > end) {
+            return new int[0];
+        }
+        for (int index = start; index <= end; index++) {
+            if (data[index] > element) {
+                indices[count++] = index;
+            }
+        }
+        return Arrays.copyOf(indices, count);
+    }
+
     public static void main(String[] args) {
         int[] data = {1, 3, 6, 8, 10};
         int element = 3;
         int start = 0;
-        int end = 3;
+        int end = 4;
         System.out.println(indexOfInRange(data, element, start, end));
+        System.out.println(Arrays.toString(indicesOfGreaterThan(data, element, start, end)));
     }
 }
 
