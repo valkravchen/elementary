8d24ea22ab3749d9a48e56ef644796269259e427
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-11-05 20:28:30 +0300

    Реализован метод findGCD для нахождения НОД двух чисел

--- src/main/java/vaok/tasks/loop/GCDCalculator.java
+++ src/main/java/vaok/tasks/loop/GCDCalculator.java
@@ -11,8 +11,8 @@ public class GCDCalculator {
     }
 
     public static void main(String[] args) {
-        System.out.println(findGCD(48, 18)); // Ожидаемый результат: 6
-        System.out.println(findGCD(54, 24)); // Ожидаемый результат: 6
+        System.out.println(findGCD(48, 18));
+        System.out.println(findGCD(54, 24));
     }
 }
 
