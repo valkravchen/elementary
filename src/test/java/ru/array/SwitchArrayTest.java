6ae356687cd40885e1cabd66f9e414da2ae9b65e
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-11-07 04:58:25 +0300

    5.11. Перестановка элементов массива

---
+++ src/test/java/ru/array/SwitchArrayTest.java
@@ -0,0 +1,16 @@
+package ru.array;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SwitchArrayTest {
+    @Test
+    void whenSwap0to3() {
+        int[] input = {1, 2, 3, 4};
+        int source = 0;
+        int destination = input.length - 1;
+        int[] result = SwitchArray.swap(input, source, destination);
+        int[] expected = {4, 2, 3, 1};
+        assertThat(result).containsExactly(expected);
+    }
+}
