f450b4177ed85a95973f89d85b896cdea8ad3c4d
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-11-08 20:29:37 +0300

    Реализован метод isPrime для проверки числа на простоту

--- src/main/java/vaok/tasks/loop/PrimeChecker.java
+++ src/main/java/vaok/tasks/loop/PrimeChecker.java
@@ -3,7 +3,7 @@ package vaok.tasks.loop;
 public class PrimeChecker {
     public static boolean isPrime(int number) {
         if (number < 2) {
-            return false; // Простые числа начинаются с 2
+            return false;
         }
         for (int i = 2; i <= Math.sqrt(number); i++) {
             if (number % i == 0) {
@@ -14,7 +14,7 @@ public class PrimeChecker {
     }
 
     public static void main(String[] args) {
-        System.out.println(isPrime(17)); // Ожидаемый результат: true
-        System.out.println(isPrime(18)); // Ожидаемый результат: false
+        System.out.println(isPrime(17));
+        System.out.println(isPrime(18));
     }
 }
