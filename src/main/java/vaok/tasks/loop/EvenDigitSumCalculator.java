80de6efa7885f858b6f4a12654898817ade1ccf9
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-11-08 20:24:10 +0300

    Реализован метод calculateEvenDigitSum для подсчёта суммы чётных цифр числа

--- src/main/java/vaok/tasks/loop/EvenDigitSumCalculator.java
+++ src/main/java/vaok/tasks/loop/EvenDigitSumCalculator.java
@@ -3,7 +3,7 @@ package vaok.tasks.loop;
 public class EvenDigitSumCalculator {
     public static int calculateEvenDigitSum(int number) {
         int sum = 0;
-        number = Math.abs(number); // Учитываем только положительные цифры
+        number = Math.abs(number);
 
         while (number > 0) {
             int digit = number % 10;
@@ -16,7 +16,7 @@ public class EvenDigitSumCalculator {
     }
 
     public static void main(String[] args) {
-        System.out.println(calculateEvenDigitSum(123456)); // Ожидаемый результат: 12
-        System.out.println(calculateEvenDigitSum(-2468));  // Ожидаемый результат: 20
+        System.out.println(calculateEvenDigitSum(123456));
+        System.out.println(calculateEvenDigitSum(-2468));
     }
 }
