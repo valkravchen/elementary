218ecc45eb8ae3b13a47f4de99973deec3defd4b
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-11-12 20:41:10 +0300

    Реализован метод countOddDivisors для подсчёта количества нечётных делителей числа

---
+++ src/main/java/vaok/tasks/loop/OddDivisorCounter.java
@@ -0,0 +1,21 @@
+package vaok.tasks.loop;
+
+public class OddDivisorCounter {
+    public static int countOddDivisors(int number) {
+        int count = 0;
+
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0 && i % 2 != 0) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countOddDivisors(12)); // Ожидаемый результат: 3
+        System.out.println(countOddDivisors(15)); // Ожидаемый результат: 4
+    }
+}
+
