fbdbd7e8ffb673b2ee408374f6031ddd76478977
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-11-20 05:50:00 +0300

    5.13. Массив заполнен true или false

--- src/main/java/ru/array/Check.java
+++ src/main/java/ru/array/Check.java
@@ -2,10 +2,11 @@ package ru.array;
 
 public class Check {
     public static void main(String[] args) {
-        boolean result = true;
         boolean[] data = new boolean[]{false, false, false};
-        for (int index = 1; index < data.length; index++) {
-            if (data[0] != data[index]) {
+        boolean result = true;
+        boolean first = data[0];
+        for (boolean value: data) {
+            if (value != first) {
                 result = false;
                 break;
             }
