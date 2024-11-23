bb2af2c209a6ebd7b4710672c8be040629e8d21b
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-11-23 17:31:39 +0300

    5.16. Обход массива с последнего элемента (теоретическая часть)

--- src/main/java/ru/array/ReverseLoopForArray.java
+++ src/main/java/ru/array/ReverseLoopForArray.java
@@ -4,8 +4,8 @@ public class ReverseLoopForArray {
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5, 6, 7};
         System.out.println("Элементы массива в обратном порядке: ");
-        for (int index = numbers.length - 1; index >= 0; index--) {
-            int element = numbers[index];
+        for (int index = 0; index < numbers.length; index++) {
+            int element = numbers[numbers.length - 1 - index];
             System.out.println(element);
         }
     }
