02c902dc4cdedc2ad1f237698160ebd4b72da6a8
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-11-24 16:51:58 +0300

    5.18. Поиск индекса в диапазоне

--- src/main/java/ru/array/FindLoop.java
+++ src/main/java/ru/array/FindLoop.java
@@ -9,4 +9,18 @@ public class FindLoop {
         }
         return -1;
     }
+
+    public static void main(String[] args) {
+        int[] data = {3, 7, 5, 25, 10, 2, 6};
+        int element = 55;
+        int start = 0;
+        int finish = 3;
+        for (int index = start; index <= finish; index++) {
+            if (element == data[index]) {
+                System.out.println(index);
+                return;
+            }
+        }
+        System.out.println("Элемента " + element + " нет в массиве data.");
+    }
 }
