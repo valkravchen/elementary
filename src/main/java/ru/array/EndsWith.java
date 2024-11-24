bc37b09ab97d93efcc0cb16b1ef652277f77f52e
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-11-24 13:15:25 +0300

    5.17. Слово заканчивается на…

--- src/main/java/ru/array/EndsWith.java
+++ src/main/java/ru/array/EndsWith.java
@@ -13,4 +13,13 @@ public class EndsWith {
         }
         System.out.println(result);
     }
+
+    public static boolean endsWith(char[] word, char[] post) {
+        for (int index = 0; index < post.length; index++) {
+            if (post[post.length - 1 - index] != word[word.length - 1 - index]) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- src/test/java/ru/array/EndsWithTest.java
+++ src/test/java/ru/array/EndsWithTest.java
@@ -6,16 +6,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class EndsWithTest {
     @Test
     public void whenEndWithPrefixThenTrue() {
-        char[] word = {'H', 'e', 'l', 'l', 'o'};
-        char[] post = {'l', 'o'};
+        char[] word = {'С', 'ч', 'а', 'с', 'т', 'ь', 'е'};
+        char[] post = {'ч', 'а', 'с', 'т', 'ь', 'е'};
         boolean result = EndsWith.endsWith(word, post);
         assertThat(result).isTrue();
     }
 
     @Test
     public void whenNotEndWithPrefixThenFalse() {
-        char[] word = {'H', 'e', 'l', 'l', 'o'};
-        char[] post = {'l', 'a'};
+        char[] word = {'С', 'ч', 'а', 'с', 'т', 'ь', 'е'};
+        char[] post = {'ч', 'а', 'с', 'т', 'ь'};
         boolean result = EndsWith.endsWith(word, post);
         assertThat(result).isFalse();
     }
