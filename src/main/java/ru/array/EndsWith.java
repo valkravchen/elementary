d5575b06a40d36ba020079b2836da9daf14095dc
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-11-24 13:17:46 +0300

    5.17. Слово заканчивается на…

--- src/main/java/ru/array/EndsWith.java
+++ src/main/java/ru/array/EndsWith.java
@@ -1,19 +1,6 @@
 package ru.array;
 
 public class EndsWith {
-    public static void main(String[] args) {
-        boolean result = true;
-        char[] word = {'Д', 'р', 'у', 'ж', 'б', 'а'};
-        char[] post = {'ж', 'б', 'а'};
-        for (int index = 0; index < post.length; index++) {
-            if (post[post.length - 1 - index] != word[word.length - 1 - index]) {
-                result = false;
-                break;
-            }
-        }
-        System.out.println(result);
-    }
-
     public static boolean endsWith(char[] word, char[] post) {
         for (int index = 0; index < post.length; index++) {
             if (post[post.length - 1 - index] != word[word.length - 1 - index]) {
