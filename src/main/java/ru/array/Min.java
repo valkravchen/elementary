f2f9116b50b7777fc9d13638bcc134fa24c3ee7c
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-11-25 19:23:14 +0300

    5.19. Поиск минимального числа в массиве

--- src/main/java/ru/array/Min.java
+++ src/main/java/ru/array/Min.java
@@ -1,11 +1,6 @@
 package ru.array;
 
 public class Min {
-    public static void main(String[] args) {
-        int[] array = new int[]{11, 3, 5, 333, 1};
-        System.out.println(findMin(array));
-    }
-
     public static int findMin(int[] array) {
         int min = array[0];
         for (int index = 1; index < array.length; index++) {
