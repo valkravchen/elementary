3401f5e5f6a6151a6400bf740fe009c120629963
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-11-26 04:47:24 +0300

    5.20. Поиск минимума в диапазоне

--- src/main/java/ru/array/MinDiapason.java
+++ src/main/java/ru/array/MinDiapason.java
@@ -2,15 +2,20 @@ package ru.array;
 
 public class MinDiapason {
     public static void main(String[] args) {
-        int[] array = new int[]{-1, 0, 5, 10};
+        int[] array = new int[]{10, 5, 3, 1};
         int start = 1;
         int finish = 3;
+        System.out.println(findMin(array, start, finish));
+    }
+
+    public static int findMin(int[] array, int start, int finish) {
         int min = array[start];
         for (int index = start + 1; index <= finish; index++) {
             if (min > array[index]) {
                 min = array[index];
             }
         }
-        System.out.println(min);
+        return min;
     }
 }
+
