95feb96b08e4518b113863a35bba2da66f38e9e6
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-11-26 19:51:16 +0300

    5.21. Сортировка выборкой

---
+++ src/main/java/ru/array/SortSelected.java
@@ -0,0 +1,10 @@
+package ru.array;
+
+public class SortSelected {
+    public static void main(String[] args) {
+        int[] array = new int[] {3, 5, 1, 2, 4};
+        int start = 0;
+        int finish = 4;
+        System.out.println(MinDiapason.findMin(array, start, finish));
+    }
+}
