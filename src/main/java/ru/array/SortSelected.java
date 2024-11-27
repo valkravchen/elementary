faddbb9558ab7ad4d5d45f5a6204cd9d617f6fe8
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-11-27 05:21:50 +0300

    5.21. Сортировка выборкой

--- src/main/java/ru/array/SortSelected.java
+++ src/main/java/ru/array/SortSelected.java
@@ -6,10 +6,19 @@ public class SortSelected {
         int start = 0;
         int finish = 4;
         int minElement = MinDiapason.findMin(array, start, finish);
-        int minElementIndex = FindLoop.indexOf(array, minElement);
+        int minElementIndex = FindLoop.indexInRange(array, minElement, start, finish);
         SwitchArray.swap(array, 0, minElementIndex);
-        for (int element : array) {
+        start = 1;
+        minElement = MinDiapason.findMin(array, start, finish);
+        minElementIndex = FindLoop.indexInRange(array, minElement, start, finish);
+        SwitchArray.swap(array, 1, minElementIndex);
+        start = 3;
+        minElement = MinDiapason.findMin(array, start, finish);
+        minElementIndex = FindLoop.indexInRange(array, minElement, start, finish);
+        SwitchArray.swap(array, 3, minElementIndex);
+        for (int element: array) {
             System.out.println(element);
         }
     }
 }
+
