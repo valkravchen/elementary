d8c3e836b16625fe44bf83f2ca3c334b27280490
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-11-27 18:12:50 +0300

    5.21. Сортировка выборкой

--- src/main/java/ru/array/SortSelected.java
+++ src/main/java/ru/array/SortSelected.java
@@ -2,9 +2,9 @@ package ru.array;
 
 public class SortSelected {
     public static void main(String[] args) {
-        int[] array = new int[] {3, 5, 1, 2, 4};
+        int[] array = new int[] {3, 5, 1, 2, 4, 0};
         int start = 0;
-        int finish = 4;
+        int finish = 5;
         int minElement = MinDiapason.findMin(array, start, finish);
         int minElementIndex = FindLoop.indexInRange(array, minElement, start, finish);
         SwitchArray.swap(array, start, minElementIndex);
@@ -17,6 +17,10 @@ public class SortSelected {
         minElementIndex = FindLoop.indexInRange(array, minElement, start, finish);
         SwitchArray.swap(array, start, minElementIndex);
         start = 3;
+        minElement = MinDiapason.findMin(array, start, finish);
+        minElementIndex = FindLoop.indexInRange(array, minElement, start, finish);
+        SwitchArray.swap(array, start, minElementIndex);
+        start = 4;
         minElement = MinDiapason.findMin(array, start, finish);
         minElementIndex = FindLoop.indexInRange(array, minElement, start, finish);
         SwitchArray.swap(array, start, minElementIndex);
