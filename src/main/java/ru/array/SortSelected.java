49f5bfcc75b6b8114f852935a76d7bfc315fc21f
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-11-28 04:54:27 +0300

    5.21. Сортировка выборкой

--- src/main/java/ru/array/SortSelected.java
+++ src/main/java/ru/array/SortSelected.java
@@ -2,31 +2,24 @@ package ru.array;
 
 public class SortSelected {
     public static void main(String[] args) {
-        int[] array = new int[] {3, 5, 1, 2, 4, 0};
-        int start = 0;
-        int finish = 5;
-        int minElement = MinDiapason.findMin(array, start, finish);
-        int minElementIndex = FindLoop.indexInRange(array, minElement, start, finish);
-        SwitchArray.swap(array, start, minElementIndex);
-        start = 1;
-        minElement = MinDiapason.findMin(array, start, finish);
-        minElementIndex = FindLoop.indexInRange(array, minElement, start, finish);
-        SwitchArray.swap(array, start, minElementIndex);
-        start = 2;
-        minElement = MinDiapason.findMin(array, start, finish);
-        minElementIndex = FindLoop.indexInRange(array, minElement, start, finish);
-        SwitchArray.swap(array, start, minElementIndex);
-        start = 3;
-        minElement = MinDiapason.findMin(array, start, finish);
-        minElementIndex = FindLoop.indexInRange(array, minElement, start, finish);
-        SwitchArray.swap(array, start, minElementIndex);
-        start = 4;
-        minElement = MinDiapason.findMin(array, start, finish);
-        minElementIndex = FindLoop.indexInRange(array, minElement, start, finish);
-        SwitchArray.swap(array, start, minElementIndex);
+        int[] array = new int[]{3, 5, 1, 2, 4, 0};
+        sort(array);
         for (int element : array) {
             System.out.println(element);
         }
     }
+
+    public static int[] sort(int[] data) {
+        for (int index = 0; index < data.length - 1; index++) {
+            int minElement = MinDiapason.findMin(data, index, data.length - 1);
+            int minElementIndex = FindLoop.indexInRange(data, minElement, index, data.length - 1);
+            if (minElementIndex == index) {
+                continue;
+            }
+            SwitchArray.swap(data, index, minElementIndex);
+        }
+        return data;
+    }
 }
+
 
