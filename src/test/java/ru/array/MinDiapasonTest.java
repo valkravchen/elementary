8ff4be95bc63c7cc3a02dbd8e8120c26f24c9b56
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-11-30 05:14:54 +0300

    Реализован метод findMinElementIndex для нахождения индекса минимального элемента

--- src/test/java/ru/array/MinDiapasonTest.java
+++ src/test/java/ru/array/MinDiapasonTest.java
@@ -11,7 +11,7 @@ class MinDiapasonTest {
         int[] array = new int[] {-1, 0, 5, 10};
         int start = 1;
         int finish = 3;
-        int result = MinDiapason.findMin(array, start, finish);
+        int result = MinDiapason.findMinElement(array, start, finish);
         int expected = 0;
         assertThat(result).isEqualTo(expected);
     }
@@ -21,7 +21,7 @@ class MinDiapasonTest {
         int[] array = new int[] {10, 5, 3, 1};
         int start = 1;
         int finish = 3;
-        int result = MinDiapason.findMin(array, start, finish);
+        int result = MinDiapason.findMinElement(array, start, finish);
         int expected = 1;
         assertThat(result).isEqualTo(expected);
     }
@@ -31,7 +31,7 @@ class MinDiapasonTest {
         int[] array = new int[] {10, 2, 5, 1};
         int start = 0;
         int finish = 2;
-        int result = MinDiapason.findMin(array, start, finish);
+        int result = MinDiapason.findMinElement(array, start, finish);
         int expected = 2;
         assertThat(result).isEqualTo(expected);
     }
