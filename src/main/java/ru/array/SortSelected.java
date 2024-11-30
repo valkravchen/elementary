bb822328de83c372006739ff028d8fcade92e6ab
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-11-30 05:29:39 +0300

    5.21. Сортировка выборкой

--- src/main/java/ru/array/SortSelected.java
+++ src/main/java/ru/array/SortSelected.java
@@ -1,14 +1,6 @@
 package ru.array;
 
 public class SortSelected {
-    public static void main(String[] args) {
-        int[] array = new int[] {3, 5, 1, 2, 4};
-        SortSelected.sort(array);
-        for (int element : array) {
-            System.out.println(element);
-        }
-    }
-
     public static int[] sort(int[] data) {
         for (int index = 0; index < data.length - 1; index++) {
             int minElementIndex = MinDiapason.findMinElementIndex(data, index, data.length - 1);
