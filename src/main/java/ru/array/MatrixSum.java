8de548eea7479a24f1ff1c9b3b6891c959107374
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-12-01 11:07:39 +0300

    5.24. Двумерный массив. Циклы

--- src/main/java/ru/array/MatrixSum.java
+++ src/main/java/ru/array/MatrixSum.java
@@ -1,14 +1,18 @@
 package ru.array;
 
 public class MatrixSum {
-    public static void main(String[] args) {
-        int[][] array = {{8}, {6, 5, 5}, {93, 3}};
-        int sum = 0;
+    public static int sum(int[][] array) {
+        int result = 0;
         for (int row = 0; row < array.length; row++) {
             for (int cell = 0; cell < array[row].length; cell++) {
-                sum += array[row][cell];
+                result += array[row][cell];
             }
         }
-        System.out.println(sum);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[][] array = {{8}, {6, 5, 5}, {93, 3}};
+        System.out.println(sum(array));
     }
 }
