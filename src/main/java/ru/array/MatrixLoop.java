20bbb0d36731fbc8788dd7a3e5872a35ada09ec5
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-12-01 09:47:07 +0300

    5.24. Двумерный массив. Циклы (теоретическая часть)

--- src/main/java/ru/array/MatrixLoop.java
+++ src/main/java/ru/array/MatrixLoop.java
@@ -4,8 +4,8 @@ public class MatrixLoop {
     public static void main(String[] args) {
         int[][] array = {{4, 2, 3}, {7, 1}, {3, 5, 6, 4}};
         for (int row = 0; row < array.length; row++) {
-            for (int column = 0; column < array[row].length; column++) {
-                int value = array[row][column];
+            for (int cell = 0; cell < array[row].length; cell++) {
+                int value = array[row][cell];
                 System.out.println(value);
             }
         }
