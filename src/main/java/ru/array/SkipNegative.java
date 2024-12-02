8bd54cd955ad1f6512f50cb80d67bb958c64483c
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-12-02 18:41:37 +0300

    5.25. Двумерный массив. Использование if

---
+++ src/main/java/ru/array/SkipNegative.java
@@ -0,0 +1,21 @@
+package ru.array;
+
+public class SkipNegative {
+    public static void main(String[] args) {
+        int[][] array = {{-33}, {23, 8, -55}, {2, 3, -77, -88}, {0}};
+        for (int row = 0; row < array.length; row++) {
+            for (int cell = 0; cell < array[row].length; cell++) {
+                if (array[row][cell] < 0) {
+                    array[row][cell] = 0;
+                }
+            }
+        }
+        for (int row = 0; row < array.length; row++) {
+            for (int cell = 0; cell < array[row].length; cell++) {
+                System.out.println(array[row][cell]);
+            }
+        }
+    }
+}
+
+
