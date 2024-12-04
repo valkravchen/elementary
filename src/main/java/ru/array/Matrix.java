096c5d893ab8ea13c6fa5bb9f973cfd38114b02c
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-12-04 20:09:09 +0300

    5.26. Двумерный массив. Таблица умножения

--- src/main/java/ru/array/Matrix.java
+++ src/main/java/ru/array/Matrix.java
@@ -10,14 +10,4 @@ public class Matrix {
         }
         return table;
     }
-
-    public static void main(String[] args) {
-        int size = 9;
-        int[][] array = Matrix.multiple(size);
-        for (int row = 0; row < array.length; row++) {
-            for (int column = 0; column < array[row].length; column++) {
-                System.out.println(array[row][column]);
-            }
-        }
-    }
 }
