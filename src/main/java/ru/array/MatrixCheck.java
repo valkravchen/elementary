86ea2337f961dfbc01223097cb529b7c3d239adc
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-12-07 13:22:37 +0300

    5.28. Моностолбец в матрице

--- src/main/java/ru/array/MatrixCheck.java
+++ src/main/java/ru/array/MatrixCheck.java
@@ -9,6 +9,14 @@ public class MatrixCheck {
         }
         return true;
     }
-}
 
+    public static boolean monoVertical(char[][] board, int column) {
+        for (int row = 0; row < board.length; row++) {
+            if (board[row][column] != 'X') {
+                return false;
+            }
+        }
+        return true;
+    }
+}
 
