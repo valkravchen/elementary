5f4ddf9a72e69ec28975f3a9c5296e973acfec3a
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-12-07 10:42:33 +0300

    5.27 Монострока в матрице

--- src/main/java/ru/array/MatrixCheck.java
+++ src/main/java/ru/array/MatrixCheck.java
@@ -2,13 +2,14 @@ package ru.array;
 
 public class MatrixCheck {
     public static boolean monoHorizontal(char[][] board, int row) {
-        int result = 0;
         for (int column = 0; column < board[row].length; column++) {
-            if (board[row][column] == 'X') {
-                result += 1;
+            if (board[row][column] != 'X') {
+                break;
             }
+
         }
-        return result == board[row].length;
+        return true;
     }
 }
+
 
