fbea5995aea226188656bdeef786a2e701fbf26a
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-12-10 19:14:40 +0300

    5.30. Выигрышные комбинации в Sokoban

--- src/main/java/ru/array/MatrixCheck.java
+++ src/main/java/ru/array/MatrixCheck.java
@@ -1,52 +1,21 @@
 package ru.array;
 
 public class MatrixCheck {
-    public static boolean monoHorizontal(char[][] board, int row) {
-        for (int column = 0; column < board[row].length; column++) {
-            if (board[row][column] != 'X') {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public static boolean monoVertical(char[][] board, int column) {
-        for (char[] row : board) {
-            if (row[column] != 'X') {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public static char[] extractDiagonal(char[][] board) {
-        char[] result = new char[board.length];
-        for (int index = 0; index < board.length; index++) {
-            result[index] = board[index][index];
-        }
-        return result;
-    }
-
     public static void main(String[] args) {
-        char[][] array = {
-                {' ', ' ', 'X', ' ', ' '},
-                {' ', ' ', 'X', ' ', ' '},
-                {' ', ' ', 'X', ' ', ' '},
-                {' ', ' ', 'X', ' ', ' '},
-                {' ', ' ', 'X', ' ', ' '},
+        char[][] input = {
+                {' ', ' ', ' '},
+                {'X', 'X', 'X'},
+                {' ', ' ', ' '},
         };
-        boolean result = isWin(array);
+        int row = 1;
+        boolean result = true;
+        for (int column = 0; column < input[row].length; column++) {
+            if (input[row][column] != 'X') {
+                result = false;
+                break;
+            }
+        }
         System.out.println(result);
     }
-
-    public static boolean isWin(char[][] board) {
-        for (int index = 0; index <= 4; index++) {
-            if (monoHorizontal(board, index) || monoVertical(board, index)) {
-                return true;
-            }
-        }
-        return false;
-    }
 }
-
 
