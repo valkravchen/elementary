01990109ce8055c2830c158976434e1550d6cf78
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-12-10 19:30:37 +0300

    5.30. Выигрышные комбинации в Sokoban

--- src/main/java/ru/array/MatrixCheck.java
+++ src/main/java/ru/array/MatrixCheck.java
@@ -1,7 +1,6 @@
 package ru.array;
 
 public class MatrixCheck {
-
     public static boolean monoHorizontal(char[][] board, int row) {
         for (int column = 0; column < board[row].length; column++) {
             if (board[row][column] != 'X') {
