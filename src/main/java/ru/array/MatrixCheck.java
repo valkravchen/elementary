613cfb077d0c162df4e8b015acff099bbdfcff9b
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-12-20 20:52:12 +0300

    5.30. Выигрышные комбинации в Sokoban

--- src/main/java/ru/array/MatrixCheck.java
+++ src/main/java/ru/array/MatrixCheck.java
@@ -44,7 +44,7 @@ public class MatrixCheck {
                 {' ', ' ', ' ', ' ', 'X'},
                 {' ', ' ', ' ', ' ', 'X'},
                 {' ', ' ', ' ', ' ', 'X'},
-                {'X', 'X', ' ', ' ', 'X'},
+                {'X', 'X', ' ', 'b', 'X'},
         };
         boolean result = isWin(array);
         System.out.println(result);
