b9bdb5e6915413beddf6d0eced0f1e3a9b3d2427
valkravchen <100293539+valkravchen@users.noreply.github.com>
2024-12-31 19:05:09 +0300

    5.31. Метод двух указателей (теория)

--- src/main/java/ru/array/Compress.java
+++ src/main/java/ru/array/Compress.java
@@ -34,10 +34,8 @@ public class Compress {
 
     public static void main(String[] args) {
         int[] array = {0, 1, 0, 0, 2, 0, 3, 0, 0, 4, 5, 6, 0, 7};
-        int[] compress = compress(array);
         int[] compress2Point = compress2Point(array);
         for (int index = 0; index < array.length; index++) {
-            System.out.println(compress[index]);
             System.out.println(compress2Point[index]);
         }
     }
