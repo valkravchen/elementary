17af15f770930576d559807da8673d4e7f67f232
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-01-08 13:29:08 +0300

    Подготовка к экзамену по базовому синтаксису Java.

---
+++ src/main/java/javabasics/syntax/JavaBasicsSyntax.java
@@ -0,0 +1,9 @@
+package javabasics.syntax;
+
+public class JavaBasicsSyntax {
+    public static void main(String[] args) {
+        if (true) {
+            System.out.println("Готовимся к экзамену.");
+        }
+    }
+}
