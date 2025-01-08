d4e423629b10237e464f8a2118507c1c3795d356
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-01-08 14:37:59 +0300

    Подготовка к экзамену по базовому синтаксису Java.

--- src/main/java/javabasics/syntax/JavaBasicsSyntax.java
+++ src/main/java/javabasics/syntax/JavaBasicsSyntax.java
@@ -3,13 +3,25 @@ package javabasics.syntax;
 public class JavaBasicsSyntax {
     public static void main(String[] args) {
         int[] array = {1, 2, 3};
+        int number = 5;
         iterateArray(array);
+        int element = findElement(array, number);
+        System.out.println(element);
     }
 
     public static void iterateArray(int[] array) {
         for (int index = 0; index < array.length; index++) {
             System.out.println(array[index]);
         }
+    }
+
+    public static int findElement(int[] array, int target) {
+        for (int element : array) {
+            if (element == target) {
+                return element;
+            }
+        }
+        return -1;
     }
 }
 
