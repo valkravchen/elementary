651f949696bca8bcfd104d980fa73e7fb9492078
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-01-14 20:44:16 +0300

    Подготовка к экзамену по базовому синтаксису Java.

--- src/main/java/javabasics/syntax/JavaBasicsSyntax.java
+++ src/main/java/javabasics/syntax/JavaBasicsSyntax.java
@@ -1,17 +1,29 @@
 package javabasics.syntax;
 
 public class JavaBasicsSyntax {
-    public void showPrimitiveTypes() {
-        byte byteNumber = 3;
-        short shortNumber = 3;
-        int intNumber = 3;
-        long longNumber = 3;
-        float floatNumber = 3.33f;
-        double doubleNumber = 3.33;
-        char charSymbol = '3';
+    public static void showPrimitiveTypes() {
+        byte byteNumber = 3; // 1 байт (8 бит)
+        short shortNumber = 3; // 2 байта (16 бит)
+        int intNumber = 3; // 4 байта (32 бита)
+        long longNumber =  2147483648L; // 8 байт (64 бита)
+        float floatNumber = 3.33f; // 4 байта (32 бита)
+        double doubleNumber = 3.33; // 8 байт (64 бита)
+        char charSymbol = '3'; // 2 байта (16 бит)
         boolean iLoveJava = true;
+
+        System.out.println("byte: " + byteNumber);
+        System.out.println("short: " + shortNumber);
+        System.out.println("int: " + intNumber);
+        System.out.println("long : " + longNumber);
+        System.out.println("float : "  + floatNumber);
+        System.out.println("double : " + doubleNumber);
+        System.out.println("char : "  + charSymbol);
+        System.out.println("bbolean : "  + iLoveJava);
     }
 
+    public static void main(String[] args) {
+        showPrimitiveTypes();
+    }
 }
 
 
