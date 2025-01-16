de86c6a50a96cf47c5c1e51d1cf9079902aa76ef
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-01-16 19:55:31 +0300

    Подготовка к экзамену по базовому синтаксису Java.

--- src/main/java/javabasics/syntax/CyclesPractice.java
+++ src/main/java/javabasics/syntax/CyclesPractice.java
@@ -17,8 +17,19 @@ public class CyclesPractice {
         System.out.println(sum);
     }
 
+    public static void findMinValue(int[] numbers) {
+       int minElement = numbers[0];
+       for (int index = 1; index < numbers.length; index++) {
+           if (minElement > numbers[index]) {
+               minElement = numbers[index];
+           }
+       }
+        System.out.println(minElement);
+    }
+
     public static void main(String[] args) {
         printNumbers();
         sumNumbers();
+        findMinValue(new int[]{2, 6, 4, 5});
     }
 }
