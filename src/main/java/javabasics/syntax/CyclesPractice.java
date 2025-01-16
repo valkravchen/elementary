01c43f2d86ff31cf94cf77ee3abc7f16e65f4b1c
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-01-16 19:35:47 +0300

    Подготовка к экзамену по базовому синтаксису Java.

--- src/main/java/javabasics/syntax/CyclesPractice.java
+++ src/main/java/javabasics/syntax/CyclesPractice.java
@@ -7,7 +7,18 @@ public class CyclesPractice {
         }
     }
 
+    public static void sumNumbers() {
+        int sum = 0;
+        int number = 1;
+        while (number < 21) {
+            sum += number;
+            number++;
+        }
+        System.out.println(sum);
+    }
+
     public static void main(String[] args) {
         printNumbers();
+        sumNumbers();
     }
 }
