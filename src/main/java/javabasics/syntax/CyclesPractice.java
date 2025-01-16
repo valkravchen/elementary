cbfa81fc38ba7b58d76bb58b8b888dc73398cb37
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-01-16 19:25:15 +0300

    Подготовка к экзамену по базовому синтаксису Java.

---
+++ src/main/java/javabasics/syntax/CyclesPractice.java
@@ -0,0 +1,13 @@
+package javabasics.syntax;
+
+public class CyclesPractice {
+    public static void printNumbers() {
+        for (int number = 1; number < 11; number++) {
+            System.out.println(number);
+        }
+    }
+
+    public static void main(String[] args) {
+        printNumbers();
+    }
+}
