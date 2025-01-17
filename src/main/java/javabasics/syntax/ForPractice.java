c68466c588c0488460a4f60942ef8ef067101203
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-01-17 19:15:40 +0300

    Подготовка к экзамену по базовому синтаксису Java.

---
+++ src/main/java/javabasics/syntax/ForPractice.java
@@ -0,0 +1,14 @@
+package javabasics.syntax;
+
+public class ForPractice {
+    public static void printReverse() {
+        for (int number = 50; number >= 1; number--) {
+            System.out.println(number);
+        }
+    }
+
+    public static void main(String[] args) {
+        printReverse();
+       ;
+    }
+}
