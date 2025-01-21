1f9e90d965946167452d2097827969f37854ba24
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-01-21 19:47:20 +0300

    Подготовка к экзамену по базовому синтаксису Java.

---
+++ src/main/java/javabasicsexam/LinearSearchWhile.java
@@ -0,0 +1,24 @@
+package javabasicsexam;
+
+public class LinearSearchWhile {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+        int target = 30;
+        int index = -1;
+        int i = 0;
+
+        while (i < numbers.length) {
+            if (numbers[i] == target) {
+                index = i;
+                break;
+            }
+            i++;
+        }
+
+        if (index != -1) {
+            System.out.println("Элемент найден на позиции: " + index);
+        } else {
+            System.out.println("Элемент не найден.");
+        }
+    }
+}
