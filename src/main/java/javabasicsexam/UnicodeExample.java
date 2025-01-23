a9ee82a32905d83c55972eeb2ea38f9403fcac03
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-01-23 19:30:06 +0300

    Подготовка к экзамену по базовому синтаксису Java.

--- src/main/java/javabasicsexam/UnicodeExample.java
+++ src/main/java/javabasicsexam/UnicodeExample.java
@@ -5,7 +5,6 @@ public class UnicodeExample {
         char letterA = 'A';          // Латинская буква A
         char digit1 = '1';           // Цифра 1
         char hanChar = '\u6F22';     // Китайский иероглиф 漢
-
         System.out.println("Латинская буква: " + letterA);
         System.out.println("Цифра: " + digit1);
         System.out.println("Иероглиф: " + hanChar);
