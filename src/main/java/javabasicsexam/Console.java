cea1a2af2b9125bc2489fd4bab6d9485086456bb
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-01-26 09:21:37 +0300

    Подготовка к экзамену по базовому синтаксису Java.

---
+++ src/main/java/javabasicsexam/Console.java
@@ -0,0 +1,12 @@
+package javabasicsexam;
+
+public class Console {
+    public static void main(String[] args) {
+        int number = 3;
+        if (number % 2 == 0) {
+            System.out.println("Число чётное");
+        } else {
+            System.out.println("Число нечётное");
+        }
+    }
+}
