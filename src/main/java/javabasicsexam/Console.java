a6872d740faa8f8c63fc5beef0e8db53ba7fb2af
valkravchen <100293539+valkravchen@users.noreply.github.com>
2025-01-26 10:01:38 +0300

    Подготовка к экзамену по базовому синтаксису Java.

--- src/main/java/javabasicsexam/Console.java
+++ src/main/java/javabasicsexam/Console.java
@@ -2,11 +2,31 @@ package javabasicsexam;
 
 public class Console {
     public static void main(String[] args) {
-        int number = 3;
-        if (number % 2 == 0) {
-            System.out.println("Число чётное");
-        } else {
-            System.out.println("Число нечётное");
-        }
+      int numberDay = 0;
+      switch (numberDay) {
+          case 1:
+              System.out.println("Понедельник");
+              break;
+          case 2:
+              System.out.println("Вторник");
+              break;
+          case 3:
+              System.out.println("Среда");
+              break;
+          case 4:
+              System.out.println("Четверг");
+              break;
+          case 5:
+              System.out.println("Пятница");
+              break;
+          case 6:
+              System.out.println("Суббота");
+              break;
+          case 7:
+              System.out.println("Воскресенье");
+              break;
+          default:
+              System.out.println("Введите число от 1 о 7!");
+      }
     }
 }
--- src/main/java/javabasicsexam/Test.java
+++ src/main/java/javabasicsexam/Test.java
@@ -2,16 +2,13 @@ package javabasicsexam;
 
 public class Test {
     public static void main(String[] args) {
-        // Символы Unicode
-        char letterA = 'A';          // Латинская буква A
-        char digit1 = '1';           // Цифра 1
-        char smiley = '\u1F60';     // Смайлик 😊
-        char hanChar = '\u6F22';     // Китайский иероглиф 漢
-
-        // Вывод символов
-        System.out.println("Латинская буква: " + letterA);
-        System.out.println("Цифра: " + digit1);
-        System.out.println("Иероглиф: " + hanChar);
-        System.out.println(smiley);
+        int target = 19;
+        int[] array = {1, 4, 34, 19, 8};
+        for (int index = 0; index < array.length; index++) {
+            if (array[index] == target) {
+                System.out.println(index);
+                break;
+            }
+        }
     }
 }
